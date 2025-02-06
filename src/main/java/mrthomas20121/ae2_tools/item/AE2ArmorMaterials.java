9502ca2a92631f2ce7da16c3b18bd8c846f61edf
Thomas B <thomas77300@gmail.com>
2025-02-06 14:38:35 +0100

    Update AE2ArmorMaterials.java

--- src/main/java/mrthomas20121/ae2_tools/item/AE2ArmorMaterials.java
+++ src/main/java/mrthomas20121/ae2_tools/item/AE2ArmorMaterials.java
@@ -1,21 +1,18 @@
 package mrthomas20121.ae2_tools.item;
 
-import appeng.core.definitions.AEItems;
 import appeng.datagen.providers.tags.ConventionTags;
 import cofh.lib.common.item.ArmorMaterialCoFH;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 
 public class AE2ArmorMaterials {
 
-    public static ArmorMaterialCoFH CERTUS_QUARTZ = new ArmorMaterialCoFH("ae2_tools:certus_quartz", 6, new int[] {3, 6, 5, 4},
+    public static ArmorMaterialCoFH CERTUS_QUARTZ = new ArmorMaterialCoFH("ae2_tools:certus_quartz", 6, new int[] {2, 5, 4, 2},
             15, SoundEvents.ARMOR_EQUIP_GOLD, 1, 0f, () -> Ingredient.of(ConventionTags.CERTUS_QUARTZ));
 
-    public static ArmorMaterialCoFH FLUIX = new ArmorMaterialCoFH("ae2_tools:fluix", 8, new int[] {4, 7, 6, 5},
-            15, SoundEvents.ARMOR_EQUIP_GOLD, 2, 0.1f, () -> Ingredient.of(ConventionTags.FLUIX_CRYSTAL));
+    public static ArmorMaterialCoFH FLUIX = new ArmorMaterialCoFH("ae2_tools:fluix", 8, new int[] {3, 6, 5, 4},
+            15, SoundEvents.ARMOR_EQUIP_TURTLE, 2, 0.1f, () -> Ingredient.of(ConventionTags.FLUIX_CRYSTAL));
 
-    public static ArmorMaterialCoFH NETHER_QUARTZ = new ArmorMaterialCoFH("ae2_tools:nether_quartz", 6, new int[] {3, 6, 5, 4},
+    public static ArmorMaterialCoFH NETHER_QUARTZ = new ArmorMaterialCoFH("ae2_tools:nether_quartz", 6, new int[] {2, 5, 4, 2},
             15, SoundEvents.ARMOR_EQUIP_GOLD, 1, 0f, () -> Ingredient.of(ConventionTags.NETHER_QUARTZ));
 }
